f943fc53d444f68ac2f515132d9481dcd4aed2fe
leanndemetro <leanndemetro@gmail.com>
2020-10-19 21:44:52 -0400

    final push

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Candolio94*",
-  database: "burgers_db"
-});
+// Global for connection
+var connection;
+// If on Heroku use the Envirnmental Variable for JawsDB
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "f2fbe0zvg9j8p9ng.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "pf5vzq6p4r28id6d",
+    password: "xck04hmcjvh5x8c7",
+    database: "n69rrmvmeezq6dy8"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
